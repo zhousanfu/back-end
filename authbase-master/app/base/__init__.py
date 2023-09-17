64a7f3efc8cb61c517878a17f2c391fa932040d1
zhousanfu <zhousanfukig@icloud.com>
2023-09-17 19:29:27 +0800

    add

--- authbase-master/app/base/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-#base = Blueprint('base', __name__, url_prefix='/base')
-base = Blueprint('base', __name__)
-from ..routes import *
--- pear-admin-flask-mini/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .file import PhotoModel
-from .rights import RightModel, RoleModel
-from .system import LogModel
-from .users import UserModel, DepartmentModel
